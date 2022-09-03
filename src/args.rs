0d5c728c41309cadf306cf18e279ed11386e1d6d
Berkay Yüksel <berkayyuksel@protonmail.com>
2022-09-03 20:19:52 +0300

    refactor: clap arguments parsing method changed from builder to derive.

--- src/args.rs
+++ src/args.rs
@@ -1,24 +1,48 @@
-use clap::{arg, command, Command, ArgMatches};
+use clap::{Parser, Subcommand};
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Db file path
+    #[clap(short, long)]
+    pub path: String,
+    #[clap(subcommand)]
+    pub command: Option<Subcommands>, // not required argument
+}
 
-pub fn arg_parse() -> ArgMatches {
-    let matches = command!()
-        .arg(arg!(--name [name] "Optional name to operate on"))
+#[derive(Subcommand)]
+pub enum Subcommands {
+    /// Get value by key from database
+    Get {
+        #[clap(short, long, action)]
+        key: String,
+    },
+    /// Insert a key value pair to database
+    Insert {
+        #[clap(short, long, action)]
+        key: String,
+        #[clap(short, long, action)]
+        value: String
+    },
+    /// Delete a key value pair from database
+    Delete {
+        #[clap(short, long, action)]
+        key: String,
+    },
+    /// Update a record from database
+    Update {
+        #[clap(short, long, action)]
+        key: String,
+        #[clap(short, long, action)]
+        value: String
+    },
+    /// test argument
+    Find {
+        #[clap(short, long, action)]
+        target: String,
+    }
+}
 
-        .subcommand(
-            Command::new("debug")
-                .about("Enables debug mode.")            
-        )
-        .subcommand(
-            Command::new("db_test")
-                .about("db test")
-                .arg(arg!(-g --get <KEY> "Get value by key.")
-                    .required(false)
-                )
-                .arg(arg!(-i --insert <KEY>"Insert some value with key to DB.")
-                    .required(false)
-                ),
-        )
-        .get_matches();
-        
-    matches
+pub fn arg_parser() -> Cli{
+    let args = Cli::parse();
+    args
 }
